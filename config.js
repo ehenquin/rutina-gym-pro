26c5f17da48a5f894ef0b82b5f12e8875006a5a4
ehenquin <167160026+ehenquin@users.noreply.github.com>
2026-05-28 21:20:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-const APPS_SCRIPT_URL = "URL_PUBLICA_DEL_APPS_SCRIPT";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyj7rZxYR5KVnpquzad0hvklgVnhMOA3Ybe4UTYWk4nNVyLZ1C0-4xYUQEM29wpFiBq/exec";
+const APPS_SCRIPT_API_KEY = "CAMBIAR_ESTA_CLAVE_PRIVADA_2026";
